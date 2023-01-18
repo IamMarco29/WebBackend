85bee9da84f7be830c281d2f091f19e5c71f8c0d
Marco <80520971+IamMarco29@users.noreply.github.com>
2023-01-18 15:15:14 +0100

    StampaMarco

--- WebBackend/src/main/java/com/GlucoseGuardian/WebBackend/WebBackendApplication.java
+++ WebBackend/src/main/java/com/GlucoseGuardian/WebBackend/WebBackendApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebBackendApplication {
+public class WebBackendApplication
+{
 
-	public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 		SpringApplication.run(WebBackendApplication.class, args);
-	}
+
+		System.out.println("Modifica di Marco");
+    }
 
 }
